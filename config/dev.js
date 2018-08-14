7839612726c11db229a3e64ce08f86e11fd45f18
Toucher-F <lianduanb@gmail.com>
2018-08-14 19:02:53 -0400

    change mongo uri

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,7 @@
 module.exports = {
   googleClientID:
-    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
-  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
-  cookieKey: '123123123'
+    "964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com",
+  googleClientSecret: "KnH-rZC23z4fr2CN4ISK4srN",
+  mongoURI: "mongodb://toucher:dl237887933@ds221292.mlab.com:21292/blog_dev",
+  cookieKey: "123123123"
 };
