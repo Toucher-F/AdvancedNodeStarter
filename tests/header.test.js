c164117dc25082e194dfba2668a529396194cb83
Toucher-F <lianduanb@gmail.com>
2018-08-16 18:40:15 -0400

    signin without oauth

--- tests/header.test.js
+++ tests/header.test.js
@@ -15,7 +15,37 @@ afterEach(async () => {
   await browser.close();
 });
 
-test("We can launch a browser", async () => {
+test("The header has the correct text", async () => {
   const text = await page.$eval("a.brand-logo", el => el.innerHTML);
   expect(text).toEqual("Blogster");
+});
+
+test("Clicking login starts oauth flow", async () => {
+  await page.click(".right a");
+
+  const url = await page.url();
+  expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test.only("When signed in, shows logout button", async () => {
+  const id = "5b75af712fbd805c08029ba7";
+
+  const Buffer = require("safe-buffer").Buffer;
+  const sessionObject = {
+    passport: {
+      user: id
+    }
+  };
+  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString(
+    "base64"
+  );
+
+  const Keygrip = require("keygrip");
+  const keys = require("../config/keys");
+  const keygrip = new Keygrip([keys.cookieKey]);
+  const sig = keygrip.sign("session=" + sessionString);
+
+  await page.setCookie({ name: "session", value: sessionString });
+  await page.setCookie({ name: "session.sig", value: sig });
+  await page.goto("localhost:3000");
 });
