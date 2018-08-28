b6406fc40ab9c0aa6d8c37d6c1f91e62aa1bf4bb
Toucher-F <lianduanb@gmail.com>
2018-08-28 17:08:48 -0400

    Add image in blog

--- config/dev.js
+++ config/dev.js
@@ -4,5 +4,7 @@ module.exports = {
   googleClientSecret: "KnH-rZC23z4fr2CN4ISK4srN",
   mongoURI: "mongodb://toucher:dl237887933@ds221292.mlab.com:21292/blog_dev",
   cookieKey: "123123123",
-  redisUrl: "redis://127.0.0.1:6379"
+  redisUrl: "redis://127.0.0.1:6379",
+  accessKeyId: " AKIAIWBHPRDEERS2Z6EQ",
+  secretAccessKey: "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"
 };
